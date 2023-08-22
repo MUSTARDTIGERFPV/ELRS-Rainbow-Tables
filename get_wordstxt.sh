ddfc2d9f415a7a721b1a54710d42defab8ff2dcd
MUSTARDTIGER <mustardtiger@thetacoduck.com>
2023-08-22 15:35:23 +0000

    Add RockYou password list

--- get_wordstxt.sh
+++ get_wordstxt.sh
@@ -4,10 +4,12 @@
 
 set -xe
 
+# Random dictionary lists
 curl --location https://github.com/dwyl/english-words/raw/master/words.txt > words.txt
 curl --location https://archive.org/download/mobywordlists03201gut/SINGLE.TXT >> words.txt
 curl --location https://archive.org/download/mobywordlists03201gut/ACRONYMS.TXT >> words.txt
 curl --location https://archive.org/download/mobywordlists03201gut/COMPOUND.TXT >> words.txt
 curl --location https://archive.org/download/mobywordlists03201gut/NAMES.TXT >> words.txt
-
+# RockYou password list
+curl --location https://github.com/brannondorsey/naive-hashcat/releases/download/data/rockyou.txt >> words.txt
 
